6907d812786298e7aa65a0d37e5065782f896e1a
Moraxyc <i@qaq.li>
2026-08-17 14:35:45 +0800

    fix(bundles.cpa): build TypeScript before install

--- pkgs/bundles/cpa/package.nix
+++ pkgs/bundles/cpa/package.nix
@@ -3,13 +3,11 @@
   fetchFromGitHub,
   buildDshBundle,
   dsh-kernel,
-  runCommand,
   nix-update-script,
 }:
 buildDshBundle (finalAttrs: {
   pname = "dsh-cpa";
   version = "0.1.4";
-  linkKernelNodeModules = dsh-kernel;
 
   src = fetchFromGitHub {
     owner = "Moraxyc";
@@ -18,20 +16,8 @@ buildDshBundle (finalAttrs: {
     hash = "sha256-52SRsynwWdzDT8x2OBGyjS7a9Zysk1lwDFm3pQ8fIYA=";
   };
 
-  npmDeps = null;
-  npmConfigHook = runCommand "noop-npm-config-hook" { } "mkdir -p $out";
-  dontNpmBuild = true;
-  dontNpmInstall = true;
-
-  installPhase = ''
-    runHook preInstall
-
-    appDir="$out/lib/node_modules/dsh-cpa"
-    mkdir -p "$appDir"
-    cp -r package.json cordis.patch.yml src "$appDir/"
-
-    runHook postInstall
-  '';
+  npmDepsHash = "sha256-ptPNi+x/GavsL8vyR4Gs9LCPvbFPQbCRiernzfAjMO8=";
+  linkKernelNodeModules = dsh-kernel;
 
   passthru.updateScript = nix-update-script {
     extraArgs = [ "--flake" ];
